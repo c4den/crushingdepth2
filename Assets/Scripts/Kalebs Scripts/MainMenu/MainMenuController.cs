29f4a68f8169ee43a6e90d96b19f8ffbab4fadcb
Kaleb Schmenk <kschmenk@outlook.com>
2023-11-03 12:58:09 -0400

    Main menu concept

---
+++ Assets/Scripts/Kalebs Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public int gameplaySceneBuildIndex = 1;
+    public GameObject mainMenuPanel;
+    public GameObject[] panels;
+
+    public void OpenPanel(GameObject panel)
+    {
+        foreach (GameObject panelObj in panels)
+        {
+            if (GameObject.ReferenceEquals(panelObj, panel) != true)
+            {
+                panelObj.SetActive(false);
+            }
+            else
+            {
+                panelObj.SetActive(true);
+            }
+        }
+    }
+
+    public void ClosePanel(GameObject panelToClose)
+    {
+        panelToClose.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void PlayGame()
+    {
+        print("Play game");
+        //SceneManager.LoadScene(gameplaySceneBuildIndex);
+    }
+
+    public void QuitGame()
+    {
+        print("Quit game");
+        Application.Quit();
+    }
+}
