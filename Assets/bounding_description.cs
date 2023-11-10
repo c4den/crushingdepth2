6969dbc2bdd79f0e00c2ad0b644db605578df597
Caden Peterson <57927858+c4den@users.noreply.github.com>
2023-11-09 22:01:14 -0500

    scenes->game_scenes->Outside_sub_sc
    Added outside section map, created bounding box for play area, added script to explain what the bounding box did.

---
+++ Assets/bounding_description.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THE CODE BELOW IS INTENTIALLY LEFT BLANK. THIS SCRIPT DOES NOTHING.
+public class bounding_description : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// This script exists to explain the purposes of the bounding box found on the outside section of the first level.
+// the game asset 'play_area_bounding_box' is made to serve as a rigidbody that prevents players from moving past it's bounds, keeping them... in bounds.
+// I think it's fairly simple, but if you find it not so, then please feel free to ask me (Caden) what the purpose is.
