87fe1b26bfb452011ce480cc572ed67208466c9c
Shawn <lilpez2000@gmail.com>
2023-11-12 19:48:53 -0500

    Hacking Door Finish

--- Assets/Scripts/BlockInteraction.cs
+++ Assets/Scripts/BlockInteraction.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class BlockInteraction : MonoBehaviour
 {
     public bool isDoorOpener; // A boolean to check if this block opens the door
+    public DoorOpenedByButton door; // Reference to the DoorOpenedByButton script
 
-    //public DoorOpenedByButton door;
     public void OnBlockClicked()
     {
         if (isDoorOpener)
         {
-            //door.OpenDoor();
-            Debug.Log("Door Open");
+            door.DoorInteract(); // Call the DoorInteract method
+            Debug.Log("Door Interaction Triggered");
         }
     }
 }
