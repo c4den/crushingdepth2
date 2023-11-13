41d1928ba999cb92ad94117c0d5aa96cd1bfa0d7
Kaleb Schmenk <kschmenk@outlook.com>
2023-11-12 21:50:25 -0500

    Sounds

--- Assets/Scripts/BlockInteraction.cs
+++ Assets/Scripts/BlockInteraction.cs
@@ -7,11 +7,20 @@ public class BlockInteraction : MonoBehaviour
     public bool isDoorOpener; // A boolean to check if this block opens the door
     public DoorOpenedByButton door; // Reference to the DoorOpenedByButton script
 
+    public AudioClip doorOpenSound;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnBlockClicked()
     {
         if (isDoorOpener)
         {
             door.DoorInteract(); // Call the DoorInteract method
+            audioSource.PlayOneShot(doorOpenSound);
             Debug.Log("Door Interaction Triggered");
         }
     }
--- Assets/Scripts/Kalebs Scripts/GenericPowerableController.cs
+++ Assets/Scripts/Kalebs Scripts/GenericPowerableController.cs
@@ -62,7 +62,7 @@ public class GenericPowerableController : MonoBehaviour, PowerableInterface
 
     void PowerSound(AudioClip clip)
     {
-
+        audioSource.PlayOneShot(clip);
     }
 
 }
